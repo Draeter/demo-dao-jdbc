df5649710c7012713456d8fb7c46253ddaec26cf
Draeter <luanfel2009@gmail.com>
2020-11-14 16:46:49 -0300

    Implementação da departmentDaoJDBC

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,2 +1,43 @@
-package application;public class Program2 {
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Program2 {
+    public static void main(String[] args) {
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("=== TEST 1: findById =======");
+        Department dep = departmentDao.findById(1);
+        System.out.println(dep);
+
+        System.out.println("\n=== TEST 2: findAll =======");
+        List<Department> list = departmentDao.findAll();
+        for(Department d : list){
+            System.out.println(d);
+        }
+
+        System.out.println("\n=== TEST 3: Insert");
+        Department newDepartment = new Department(null, "Music");
+        departmentDao.insert(newDepartment);
+        System.out.println("Inserted! New id: " + newDepartment.getId());
+
+        System.out.println("=== TEST 4: update ====");
+        Department dep2 = departmentDao.findById(1);
+        dep2.setName("Food");
+        departmentDao.update(dep2);
+        System.out.println("Updated Completed");
+
+        System.out.println("=== TEST 5: delete ====");
+        System.out.print("Enter id for delete test: ");
+        int id = sc.nextInt();
+        departmentDao.deleteById(id);
+        System.out.println("Delete completed");
+
+        sc.close();
+    }
 }
